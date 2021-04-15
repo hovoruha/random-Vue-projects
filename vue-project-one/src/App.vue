b7f2bf97f8faa97e8f90309da59806c245f52225
Octavian Hovoruha <octavian.hovoruha@gmail.com>
2021-04-16 00:10:16 +0300

    initiate sidebar

--- vue-project-one/src/App.vue
+++ vue-project-one/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="wrapper">
     <app-navbar />
+    <app-sidebar />
   </div>
 </template>
 
 <script>
 import appNavbar from "./components/navbar";
+import appSidebar from "./components/sidebar";
 
 export default {
   name: "App",
 
   components: {
     appNavbar,
+    appSidebar,
   },
 };
 </script>
---
+++ vue-project-one/src/components/sidebar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app-sidebar"></div>
+</template>
+
+<script>
+export default {
+  name: "sideBar",
+
+  data() {
+    return {
+      sidebarInputs: [{ label: "", type: "", placeholder: "" }],
+    };
+  },
+};
+</script>
